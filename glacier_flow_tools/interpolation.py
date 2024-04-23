Fixed normal component calculation
Added tests for profile extraction.

glacier_flow_tools/interpolation.py
@@ -387,7 +387,7 @@ class InterpolationMatrix:
         A = self.A.tocsr()
         n_points = A.shape[0]
 
-        output_mask = np.zeros(n_points, dtype=np.bool_)
+        output_mask = np.zeros(n_points, dtype=bool)
 
         for r in range(n_points):
             # for each row, i.e. each point along the profile

glacier_flow_tools/profiles.py
@@ -18,12 +18,145 @@
 """
 Module provides profile functions.
 """
+from pathlib import Path
 from typing import List, Union
 
+import cartopy.crs as ccrs
+import geopandas as gp
 import numpy as np
 import pylab as plt
 import seaborn as sns
 import xarray as xr
+from matplotlib import cm, colors
+from matplotlib.colors import LightSource
+from shapely import get_coordinates
+
+from glacier_flow_tools.utils import blend_multiply, figure_extent, get_dataarray_extent
+
+
+def plot_profile(ds: xr.Dataset, result_dir: Path, alpha: float = 0.0, sigma: float = 1.0):
+    """
+    Plot a profile dataset created with ds.profiles.extract_profile.
+
+    This function plots a profile dataset that was created with the `extract_profile` method of the `profiles`
+    attribute of an `xr.Dataset` object. The plot is saved as a PDF file in the specified result directory.
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        The profile dataset to be plotted.
+    result_dir : Path
+        The directory where the result PDF file will be saved.
+    alpha : float, optional
+        The alpha value to be used for the plot, which determines the transparency of the plot, by default 0.0.
+    sigma : float, optional
+        The sigma value to be used for the plot, which determines the width of the Gaussian kernel, by default 1.0.
+    """
+
+    fig = ds.profiles.plot(palette="Greens", sigma=sigma, alpha=alpha)
+    profile_name = ds["profile_name"].values[0]
+    fig.savefig(result_dir / f"{profile_name}_profile.pdf")
+    plt.close()
+    del fig
+
+
+def plot_glacier(
+    profile_series: gp.GeoSeries,
+    surface: xr.DataArray,
+    overlay: xr.DataArray,
+    result_dir: Union[str, Path],
+    cmap="viridis",
+    vmin: float = 10,
+    vmax: float = 1500,
+    ticks: Union[List[float], np.ndarray] = [10, 100, 250, 500, 750, 1500],
+):
+    """
+    Plot a surface over a hillshade, add profile and correlation coefficient.
+
+    This function plots a surface over a hillshade, adds a profile and correlation coefficient.
+    The plot is saved as a PDF file in the specified result directory.
+
+    Parameters
+    ----------
+    profile_series : gp.GeoSeries
+        The profile to be plotted.
+    surface : xr.DataArray
+        The surface to be plotted over the hillshade.
+    overlay : xr.DataArray
+        The overlay to be added to the plot.
+    result_dir : Union[str, Path]
+        The directory where the result PDF file will be saved.
+    cmap : str, optional
+        The colormap to be used for the plot, by default "viridis".
+    vmin : float, optional
+        The minimum value for the colormap, by default 10.
+    vmax : float, optional
+        The maximum value for the colormap, by default 1500.
+    ticks : Union[List[float], np.ndarray], optional
+        The ticks to be used for the colorbar, by default [10, 100, 250, 500, 750, 1500].
+    """
+
+    geom = getattr(profile_series, "geometry")
+    geom_centroid = geom.centroid
+    profile_centroid = gp.GeoDataFrame([profile_series], geometry=[geom_centroid])
+    profile = gp.GeoDataFrame([profile_series], geometry=[geom])
+    glacier_name = getattr(profile, "profile_name").values[0]
+    exp_id = getattr(profile, "exp_id").values[0]
+    geom = getattr(profile, "geometry")
+    x, y = get_coordinates(geom).T
+    x_c, y_c = round(x), round(y)
+    extent_slice = figure_extent(x_c, y_c)
+    cartopy_crs = ccrs.NorthPolarStereo(central_longitude=-45, true_scale_latitude=70, globe=None)
+    # Shade from the northwest, with the sun 45 degrees from horizontal
+    light_source = LightSource(azdeg=315, altdeg=45)
+    glacier_overlay = overlay.sel(extent_slice)
+    glacier_surface = surface.interp_like(glacier_overlay)
+
+    extent = get_dataarray_extent(glacier_overlay)
+    norm = colors.Normalize(vmin=vmin, vmax=vmax)
+    mapper = cm.ScalarMappable(norm=norm, cmap=cmap)
+
+    v = mapper.to_rgba(glacier_overlay.to_numpy())
+    z = glacier_surface.to_numpy()
+    fig = plt.figure(figsize=(6.2, 6.2))
+    ax = fig.add_subplot(111, projection=cartopy_crs)
+    rgb = light_source.shade_rgb(v, elevation=z, vert_exag=0.01, blend_mode=blend_multiply)
+    # Use a proxy artist for the colorbar...
+    im = ax.imshow(v, cmap=cmap, vmin=vmin, vmax=vmax)
+    im.remove()
+    corr = ax.imshow(
+        v,
+        vmin=0,
+        vmax=1,
+        cmap="RdYlGn",
+    )
+    corr.remove()
+    ax.imshow(rgb, extent=extent, origin="upper", transform=cartopy_crs)
+    profile.plot(ax=ax, color="k", lw=1)
+    profile_centroid.plot(
+        column="pearson_r", vmin=0, vmax=1, cmap="RdYlGn", markersize=50, legend=False, missing_kwds={}, ax=ax
+    )
+    ax.annotate(f"{glacier_name}", (x_c, y_c), (10, 10), xycoords="data", textcoords="offset points")
+    ax.gridlines(
+        draw_labels={"top": "x", "left": "y"},
+        dms=True,
+        xlocs=np.arange(-50, 0, 1),
+        ylocs=np.arange(50, 88, 1),
+        x_inline=False,
+        y_inline=False,
+        rotate_labels=20,
+        ls="dotted",
+        color="k",
+    )
+
+    ax.set_extent(extent, crs=cartopy_crs)
+    fig.colorbar(im, ax=ax, shrink=0.5, pad=0.025, label=overlay.units, extend="both", ticks=ticks)
+    fig.colorbar(
+        corr, ax=ax, shrink=0.5, pad=0.025, label="Pearson $r$ (1)", orientation="horizontal", location="bottom"
+    )
+    fig.savefig(result_dir / Path(f"{glacier_name}_{exp_id}_speed.pdf"))
+    plt.close()
+    del fig
 
 
 def normal(point0: np.ndarray, point1: np.ndarray) -> np.ndarray:
@@ -243,15 +376,14 @@ def process_profile(
         -----
         This function uses the 'extract_profile' method of the 'profiles' accessor of the input dataset.
         """
-        ds_profile = ds.profiles.extract_profile(
-            x, y, profile_name=profile_name, profile_id=profile_id, normal_var=obs_normal_var, **kwargs
-        )
+        ds_profile = ds.profiles.extract_profile(x, y, profile_name=profile_name, profile_id=profile_id, **kwargs)
         return ds_profile
 
     obs_profile = extract_and_prepare(
         obs_ds,
         profile_name=profile_name,
         profile_id=profile_id,
+        normal_var=obs_normal_var,
         normal_error_var=obs_normal_error_var,
         normal_component_vars=obs_normal_component_vars,
         normal_component_error_vars=obs_normal_component_error_vars,
@@ -324,7 +456,52 @@ class CustomDatasetMethods:
             The name of the y component variable in the Dataset, by default "vy".
         normal_name : str, optional
             The name of the normal component variable to add to the Dataset, by default "v_normal".
+
+        Returns
+        -------
+        xr.Dataset
+            The xarray Dataset with the normal variables added.
         """
+        assert (x_component and y_component) in self._obj.data_vars
+
+        def func(x, x_n, y, y_n):
+            """
+            Calculate the normal component of a vector.
+
+            This function computes the normal component of a vector by performing a dot product operation.
+            The inputs are the x and y components of the vector and their corresponding normal components.
+
+            Parameters
+            ----------
+            x : float
+                The x-component of the vector.
+            x_n : float
+                The x-component of the normal.
+            y : float
+                The y-component of the vector.
+            y_n : float
+                The y-component of the normal.
+
+            Returns
+            -------
+            float
+                The normal component of the vector.
+
+            Examples
+            --------
+            >>> func(1, 2, 3, 4)
+            14
+            """
+            return x * x_n + y * y_n
+
+        self._obj[normal_name] = xr.apply_ufunc(
+            func,
+            self._obj[x_component],
+            self._obj["nx"],
+            self._obj[y_component],
+            self._obj["ny"],
+        )
+        return self._obj
 
     def calculate_stats(
         self,
@@ -515,21 +692,21 @@ class CustomDatasetMethods:
 
         if compute_profile_normal:
 
-            a = [(v in v.data_vars) for v in normal_component_vars.values()]
-            assert np.alltrue(np.array(a))  # type: ignore[attr-defined]
-            a = [(v in v.data_vars) for v in normal_component_error_vars.values()]
-            assert np.alltrue(np.array(a))  # type: ignore[attr-defined]
+            a = [(v in ds.data_vars) for v in normal_component_vars.values()]
+            if np.all(np.array(a)):
+                ds.profiles.add_normal_component(
+                    x_component=normal_component_vars["x"],
+                    y_component=normal_component_vars["y"],
+                    normal_name=normal_var,
+                )
 
-            ds.profiles.add_normal_component(
-                x_component=normal_component_vars["x"],
-                y_component=normal_component_vars["y"],
-                normal_name=normal_var,
-            )
-            ds.profiles.add_normal_component(
-                x_component=normal_component_error_vars["x"],
-                y_component=normal_component_error_vars["y"],
-                normal_name=normal_error_var,
-            )
+            a = [(v in ds.data_vars) for v in normal_component_error_vars.values()]
+            if np.all(np.array(a)):
+                ds.profiles.add_normal_component(
+                    x_component=normal_component_error_vars["x"],
+                    y_component=normal_component_error_vars["y"],
+                    normal_name=normal_error_var,
+                )
         return ds
 
     def plot(

profiles/compute_profiles.py
@@ -7,7 +7,7 @@
 # Foundation; either version 3 of the License, or (at your option) any later
 # version.
 #
-# PISM-RAGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+# GLACIER-FLOW-TOOLS is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 # details.
@@ -22,199 +22,25 @@ Calculate proifles and compute statistics along profiles.
 
 import time
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+from functools import partial
 from pathlib import Path
-from typing import Dict, List, Union
+from typing import List
 
-import cartopy.crs as ccrs
 import dask_geopandas
-import fsspec
 import geopandas as gp
-import numpy as np
-import pylab as plt
 import xarray as xr
 from dask import dataframe as dd
 from dask.distributed import Client, LocalCluster, progress
 from joblib import Parallel, delayed
-from matplotlib import cm, colors
-from matplotlib.colors import LightSource
 from tqdm.auto import tqdm
 
-from glacier_flow_tools.profiles import process_profile
+from glacier_flow_tools.profiles import plot_glacier, plot_profile, process_profile
 from glacier_flow_tools.utils import (
-    blend_multiply,
     merge_on_intersection,
     preprocess_nc,
     qgis2cmap,
     tqdm_joblib,
 )
-
-
-def figure_extent(x_c: float, y_c: float, x_e: float = 50_000, y_e: float = 50_000) -> Dict[str, slice]:
-    """
-    Calculate bounding box (figure extent) given center coordinates and x,y half-width/height.
-
-    This function calculates the bounding box (figure extent) for a figure given the center coordinates
-    and the half-width and half-height in the x and y directions, respectively.
-
-    Parameters
-    ----------
-    x_c : float
-        The x-coordinate of the center of the figure.
-    y_c : float
-        The y-coordinate of the center of the figure.
-    x_e : float, optional
-        The half-width of the figure in the x direction, by default 50_000.
-    y_e : float, optional
-        The half-height of the figure in the y direction, by default 50_000.
-
-    Returns
-    -------
-    Dict[str, slice]
-        A dictionary with keys 'x' and 'y' and values that are slices representing the extent of the figure.
-    """
-    return {"x": slice(x_c - x_e / 2, x_c + x_e / 2), "y": slice(y_c + y_e / 2, y_c - y_e / 2)}
-
-
-def plot_profile(ds: xr.Dataset, result_dir: Path, alpha: float = 0.0, sigma: float = 1.0):
-    """
-    Plot a profile dataset created with ds.profiles.extract_profile.
-
-    This function plots a profile dataset that was created with the `extract_profile` method of the `profiles`
-    attribute of an `xr.Dataset` object. The plot is saved as a PDF file in the specified result directory.
-
-    Parameters
-    ----------
-    ds : xr.Dataset
-        The profile dataset to be plotted.
-    result_dir : Path
-        The directory where the result PDF file will be saved.
-    alpha : float, optional
-        The alpha value to be used for the plot, which determines the transparency of the plot, by default 0.0.
-    sigma : float, optional
-        The sigma value to be used for the plot, which determines the width of the Gaussian kernel, by default 1.0.
-    """
-
-    fig = ds.profiles.plot(palette="Greens", sigma=sigma, alpha=alpha)
-    profile_name = ds["profile_name"].values[0]
-    fig.savefig(result_dir / f"{profile_name}_profile.pdf")
-    plt.close()
-    del fig
-
-
-def get_extent(ds: xr.DataArray) -> List[float]:
-    """
-    Get the extent of the data array.
-
-    This function returns the extent (the minimum and maximum values) of the 'x' and 'y' dimensions
-    of the input data array.
-
-    Parameters
-    ----------
-    ds : xr.DataArray
-        The input data array.
-
-    Returns
-    -------
-    List[float]
-        The extent of the data array, in the format [xmin, xmax, ymax, ymin].
-    """
-    return [ds["x"].values[0], ds["x"].values[-1], ds["y"].values[-1], ds["y"].values[0]]
-
-
-def plot_glacier(
-    profile: gp.GeoDataFrame,
-    surface: xr.DataArray,
-    overlay: xr.DataArray,
-    result_dir: Union[str, Path],
-    cmap="viridis",
-    vmin: float = 10,
-    vmax: float = 1500,
-    ticks: Union[List[float], np.ndarray] = [10, 100, 250, 500, 750, 1500],
-):
-    """
-    Plot a surface over a hillshade, add profile and correlation coefficient.
-
-    This function plots a surface over a hillshade, adds a profile and correlation coefficient.
-    The plot is saved as a PDF file in the specified result directory.
-
-    Parameters
-    ----------
-    profile : gp.GeoDataFrame
-        The profile to be plotted.
-    surface : xr.DataArray
-        The surface to be plotted over the hillshade.
-    overlay : xr.DataArray
-        The overlay to be added to the plot.
-    result_dir : Union[str, Path]
-        The directory where the result PDF file will be saved.
-    cmap : str, optional
-        The colormap to be used for the plot, by default "viridis".
-    vmin : float, optional
-        The minimum value for the colormap, by default 10.
-    vmax : float, optional
-        The maximum value for the colormap, by default 1500.
-    ticks : Union[List[float], np.ndarray], optional
-        The ticks to be used for the colorbar, by default [10, 100, 250, 500, 750, 1500].
-    """
-
-    profile_centroid = gp.GeoDataFrame(profile, geometry=profile.geometry.centroid)
-    glacier_name = profile.iloc[0]["profile_name"]
-    exp_id = profile.iloc[0]["exp_id"]
-    x_c = round(profile_centroid.geometry.x.values[0])
-    y_c = round(profile_centroid.geometry.y.values[0])
-    extent_slice = figure_extent(x_c, y_c)
-    cartopy_crs = ccrs.NorthPolarStereo(central_longitude=-45, true_scale_latitude=70, globe=None)
-    # Shade from the northwest, with the sun 45 degrees from horizontal
-    light_source = LightSource(azdeg=315, altdeg=45)
-    glacier_overlay = overlay.sel(extent_slice)
-    glacier_surface = surface.interp_like(glacier_overlay)
-
-    extent = get_extent(glacier_overlay)
-    norm = colors.Normalize(vmin=vmin, vmax=vmax)
-    mapper = cm.ScalarMappable(norm=norm, cmap=cmap)
-
-    v = mapper.to_rgba(glacier_overlay.to_numpy())
-    z = glacier_surface.to_numpy()
-    fig = plt.figure(figsize=(6.2, 6.2))
-    ax = fig.add_subplot(111, projection=cartopy_crs)
-    rgb = light_source.shade_rgb(v, elevation=z, vert_exag=0.01, blend_mode=blend_multiply)
-    # Use a proxy artist for the colorbar...
-    im = ax.imshow(v, cmap=cmap, vmin=vmin, vmax=vmax)
-    im.remove()
-    corr = ax.imshow(
-        v,
-        vmin=0,
-        vmax=1,
-        cmap="RdYlGn",
-    )
-    corr.remove()
-    ax.imshow(rgb, extent=extent, origin="upper", transform=cartopy_crs)
-    profile.plot(ax=ax, color="k", lw=1)
-    profile_centroid.plot(
-        column="pearson_r", vmin=0, vmax=1, cmap="RdYlGn", markersize=50, legend=False, missing_kwds={}, ax=ax
-    )
-    ax.annotate(f"{glacier_name}", (x_c, y_c), (10, 10), xycoords="data", textcoords="offset points")
-    ax.gridlines(
-        draw_labels={"top": "x", "left": "y"},
-        dms=True,
-        xlocs=np.arange(-50, 0, 1),
-        ylocs=np.arange(50, 88, 1),
-        x_inline=False,
-        y_inline=False,
-        rotate_labels=20,
-        ls="dotted",
-        color="k",
-    )
-
-    ax.set_extent(extent, crs=cartopy_crs)
-    fig.colorbar(im, ax=ax, shrink=0.5, pad=0.025, label=overlay.units, extend="max", ticks=ticks)
-    fig.colorbar(
-        corr, ax=ax, shrink=0.5, pad=0.025, label="Pearson $r$ (1)", orientation="horizontal", location="bottom"
-    )
-    fig.savefig(result_dir / Path(f"{glacier_name}_{exp_id}_speed.pdf"))
-    plt.close()
-    del fig
-
 
 if __name__ == "__main__":
 
@@ -270,24 +96,28 @@ if __name__ == "__main__":
     profiles_gp = gp.GeoDataFrame(profiles_gp, geometry=geom)
     profiles_gp = profiles_gp[["profile_id", "profile_name", "geometry"]]
 
-    fs = fsspec.filesystem("https")
-    if options.velocity_url:
-        is_url = options.velocity_url.split(":")[0] == ("https" or "http")
-        if is_url:
-            velocity_file = fs.open(options.velocity_url)
-        else:
-            velocity_file = Path(options.velocity_url)
-
-        velocity_ds = xr.open_dataset(velocity_file, chunks="auto")
+    velocity_file = Path(options.velocity_url)
+    velocity_ds = xr.open_dataset(velocity_file, chunks="auto", decode_times=False)
 
     if options.thickness_url:
         thickness_file = Path(options.thickness_url)
         thickness_ds = xr.open_dataset(thickness_file, chunks="auto")
 
+    print("Opening experiments")
     exp_files = [Path(x) for x in options.INFILES]
+    start = time.time()
     exp_ds = xr.open_mfdataset(
-        exp_files, preprocess=preprocess_nc, concat_dim="exp_id", combine="nested", chunks="auto", parallel=True
+        exp_files,
+        preprocess=partial(preprocess_nc, drop_dims=["z", "zb"]),
+        concat_dim="exp_id",
+        combine="nested",
+        chunks="auto",
+        engine="h5netcdf",
+        parallel=True,
+        decode_times=False,
     )
+    time_elapsed = time.time() - start
+    print(f"Time elapsed {time_elapsed:.0f}s")
 
     qgis_colormap = Path("../data/speed-colorblind.txt")
     overlay_cmap = qgis2cmap(qgis_colormap, name="speeds")
@@ -299,85 +129,66 @@ if __name__ == "__main__":
     n_jobs = len(client.ncores())
     print(f"Open client in browser: {client.dashboard_link}")
 
-    start = time.time()
-    velocity_ds_scattered = client.scatter(velocity_ds)
-    exp_ds_scattered = client.scatter(exp_ds)
-    futures = []
-    for _, p in profiles_gp.iterrows():
-        future = client.submit(process_profile, p, velocity_ds_scattered, exp_ds_scattered, stats=stats)
-        futures.append(future)
+    with client:
+        start = time.time()
+        velocity_ds_scattered = client.scatter(velocity_ds)
+        exp_ds_scattered = client.scatter(exp_ds)
+        futures = []
+        for _, p in profiles_gp.iterrows():
+            future = client.submit(process_profile, p, velocity_ds_scattered, exp_ds_scattered, stats=stats)
+            futures.append(future)
 
-    futures_computed = client.compute(futures)
-    progress(futures_computed)
-    obs_sims_profiles = client.gather(futures_computed)
+        futures_computed = client.compute(futures)
+        progress(futures_computed)
+        obs_sims_profiles = client.gather(futures_computed)
 
-    time_elapsed = time.time() - start
-    print(f"Time elapsed {time_elapsed:.0f}s")
+        time_elapsed = time.time() - start
+        print(f"Time elapsed {time_elapsed:.0f}s")
 
-    n_partitions = 2
-    profiles = dask_geopandas.from_geopandas(
-        gp.GeoDataFrame(profiles_gp, geometry=profiles_gp.geometry), npartitions=n_partitions
-    )
-
-    def concat(profiles_df, profiles_ds):
-        """
-        Concatenate a merged profiles
-        """
-        return dd.concat(
-            [
-                merge_on_intersection(profiles_df, p.mean(["profile_axis"], skipna=True).to_dask_dataframe())
-                for p in profiles_ds
-            ]
+        n_partitions = 1
+        profiles = dask_geopandas.from_geopandas(
+            gp.GeoDataFrame(profiles_gp, geometry=profiles_gp.geometry), npartitions=n_partitions
         )
 
-    print("Merging dataframes")
-    start = time.time()
-    stats_profiles = (
-        dd.concat(
-            [
-                merge_on_intersection(profiles, p.mean(["profile_axis"], skipna=True).to_dask_dataframe())
-                for p in obs_sims_profiles
-            ]
-        )
-        .compute()
-        .reset_index(drop=True)
-    )
+        def concat(profiles_df, profiles_ds):
+            """
+            Concatenate a merged profiles
+            """
+            return dd.concat(
+                [
+                    merge_on_intersection(profiles_df, p.mean(["profile_axis"], skipna=True).to_dask_dataframe())
+                    for p in profiles_ds
+                ]
+            )
 
-    # profiles_scattered = client.scatter(profiles)
-    # obs_sims_profiles_scattered = client.scatter(obs_sims_profiles)
-    # futures = client.submit(concat, profiles_scattered, obs_sims_profiles_scattered)
-    # progress(futures)
-    # stats_profiles = client.gather(futures)
-    # stats_profiles = stats_profiles.compute().reset_index(drop=True)
+        print("Merging dataframes")
+        start = time.time()
 
-    time_elapsed = time.time() - start
-    print(f"Time elapsed {time_elapsed:.0f}s")
+        profiles_scattered = client.scatter(profiles)
+        obs_sims_profiles_scattered = client.scatter(obs_sims_profiles)
+        futures = client.submit(concat, profiles_scattered, obs_sims_profiles_scattered)
+        progress(futures)
+        stats_profiles = client.gather(futures).compute().reset_index(drop=True)
+
+        time_elapsed = time.time() - start
+        print(f"Time elapsed {time_elapsed:.0f}s")
 
     gris_ds = xr.open_dataset(Path("/Users/andy/Google Drive/My Drive/data/MCdataset/BedMachineGreenland-v5.nc"))
     surface_da = gris_ds["surface"]
     overlay_da = velocity_ds["v"].where(velocity_ds["ice"])
 
     start = time.time()
-    # print("hi")
-    # for k, s in enumerate(stats_profiles.iterrows()):
-    #     profile = stats_profiles[stats_profiles.index == k]
-    #     plot_glacier(stats_profiles[stats_profiles.index == k],
-    #             surface_da,
-    #             overlay_da,
-    #             profile_result_dir,
-    #             cmap=overlay_cmap,
-    #         )
-
+    print("Plotting glaciers")
     with tqdm_joblib(tqdm(desc="Plotting glaciers", total=len(stats_profiles))) as progress_bar:
         Parallel(n_jobs=n_jobs)(
             delayed(plot_glacier)(
-                stats_profiles[stats_profiles.index == k],
+                p,
                 surface_da,
                 overlay_da,
                 profile_result_dir,
                 cmap=overlay_cmap,
             )
-            for k, _ in enumerate(stats_profiles.iterrows())
+            for _, p in stats_profiles.iterrows()
         )
     time_elapsed = time.time() - start
     print(f"Time elapsed {time_elapsed:.0f}s")
@@ -387,5 +198,3 @@ if __name__ == "__main__":
             delayed(plot_profile)(ds, profile_result_dir, alpha=obs_scale_alpha, sigma=obs_sigma)
             for ds in obs_sims_profiles
         )
-
-    client.close()

tests/test_interpolation.py
@@ -64,7 +64,7 @@ def test_masked_missing_interpolation():
     # set the four elements in the corner to nan and mark them as
     # missing
     z[0:2, 0:2] = np.nan
-    mask = np.zeros_like(z, dtype=np.bool_)
+    mask = np.zeros_like(z, dtype=bool)
     mask[0:2, 0:2] = np.nan
 
     z = np.ma.array(z, mask=mask)